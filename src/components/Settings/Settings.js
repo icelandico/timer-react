3ae7deca671006dabebd89db33993d1f92019098
Michal Muszynski <m.muszy@gmail.com>
2018-12-07 13:15:48 +0100

    Styling settings input

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Settings.css'
 
 class Settings extends Component {
 
@@ -14,29 +15,38 @@ class Settings extends Component {
       <div>
         <div>
           <h2>Set Pomodoro time</h2>
+          <div className="timer-container">
           <p>Minutes</p>
           <input type="text"
                  className="input-time"
                  value={this.state.timerMinutes}
           />
+          </div>
+          <div className="timer-container">
           <p>Seconds</p>
           <input type="text"
                  className="input-time"
                  value={this.state.timerSeconds}
           />
+          </div>
         </div>
         <div>
           <h2>Set break time</h2>
+          <div className="timer-container">
           <p>Minutes</p>
           <input type="text"
                  className="input-time"
                  value={this.state.breakMinutes}
           />
+          </div>
+          <div className="timer-container">
           <p>Seconds</p>
           <input type="text"
                  className="input-time"
                  value={this.state.breakSeconds}
           />
+          </div>
+
         </div>
       </div>
     )
