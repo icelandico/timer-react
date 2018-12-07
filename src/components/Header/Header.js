001ab95679e3133750703d16accf34029aad187a
Michal Muszynski <m.muszy@gmail.com>
2018-12-07 10:47:18 +0100

    Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return(
+    <div className="header-container">
+      <h1>Timer App</h1>
+    </div>
+  )
+}
+
+export default Header
