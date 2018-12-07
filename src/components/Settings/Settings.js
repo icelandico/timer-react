dbd85bbf46d9ad3619888b76f163c87b48c38816
Michal Muszynski <m.muszy@gmail.com>
2018-12-07 12:31:51 +0100

    Settings panel

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Settings extends Component {
+  render() {
+    return(
+      <div>
+        <div>
+          <h2>Set Pomodoro time</h2>
+        </div>
+        <div>
+          <h2>Set break time</h2>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Settings
