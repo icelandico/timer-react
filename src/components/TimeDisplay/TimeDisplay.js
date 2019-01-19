c7c4888397a14d199b04a8dd85ca8ca24a54a86b
Michal Muszynski <m.muszy@gmail.com>
2019-01-19 20:57:34 +0100

    Code cleanup

--- src/components/TimeDisplay/TimeDisplay.js
+++ src/components/TimeDisplay/TimeDisplay.js
@@ -3,7 +3,9 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { TimeDisplayStyles } from './TimeDisplay-styles';
 import PropTypes from 'prop-types';
 
-  function leadingZero(number) {
+class TimeDisplayer extends Component {
+
+  leadingZero = (number) => {
     if (number > 9) {
       return number
     } else if (!number) {
@@ -13,32 +15,20 @@ import PropTypes from 'prop-types';
     }
   }
 
-  class TimeDisplayer extends Component {
-
-    leadingZero = (number) => {
-      if (number > 9) {
-        return number
-      } else if (!number) {
-        return '00'
-      } else {
-        return ('0' + number).slice(-2)
-      }
-    }
-
-    get classes() {
-      return this.props.classes
-    }
-
-    render() {
-      const { isBreak, minutes, seconds, breakMinutes, breakSeconds } = this.props
-      return (
-        <div className={this.classes.timerDisplay}>
-          <p className={isBreak ? `${this.classes.timeValue} ${this.classes.break}` : this.classes.timeValue}>{leadingZero(minutes)} : {seconds === '60' ? '00' : leadingZero(seconds)}</p>
-          <p className={isBreak ? this.classes.timeValue : `${this.classes.timeValue} ${this.classes.break}`}>{leadingZero(breakMinutes)} : {breakSeconds === '60' ? '00' : leadingZero(breakSeconds)}</p>
-        </div>
-      );
-    }
+  get classes() {
+    return this.props.classes
   }
+
+  render() {
+    const { isBreak, minutes, seconds, breakMinutes, breakSeconds } = this.props
+    return (
+      <div className={this.classes.timerDisplay}>
+        <p className={isBreak ? `${this.classes.timeValue} ${this.classes.break}` : this.classes.timeValue}>{leadingZero(minutes)} : {seconds === '60' ? '00' : leadingZero(seconds)}</p>
+        <p className={isBreak ? this.classes.timeValue : `${this.classes.timeValue} ${this.classes.break}`}>{leadingZero(breakMinutes)} : {breakSeconds === '60' ? '00' : leadingZero(breakSeconds)}</p>
+      </div>
+    );
+  }
+}
   
 
 TimeDisplayer.propTypes = {
